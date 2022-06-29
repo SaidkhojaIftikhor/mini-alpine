d538032d086b427180034d00355736dca0c96f61
SaidkhojaIftikhor <saidkhojaiftikhor2002@gmail.com>
2022-06-29 09:13:35 +0500

    add eslint + prettier

--- Alpine.js
+++ Alpine.js
@@ -1,106 +1,109 @@
 window.Alpine = {
-  cache: new Map(),
+    cache: new Map(),
 
-  constants: {
-    removedElement: "_rmEl",
-  },
-  directives: {
-    "x-text": (el, value) => {
-      el.textContent = value;
+    constants: {
+        removedElement: "_rmEl",
+    },
+    directives: {
+        "x-text": (el, value) => {
+            el.textContent = value;
+        },
+
+        "x-show": (el, value) => {
+            el.style.display = value ? "block" : "none";
+        },
+        "x-if": (element, value) => {
+            if (!value) {
+                if (element instanceof HTMLElement) {
+                    if (!Alpine.cache.has(Alpine.constants.removedElement)) {
+                        Alpine.cache.set(
+                            Alpine.constants.removedElement,
+                            element.innerHTML.toString()
+                        );
+                    }
+
+                    element.innerHTML = "";
+                }
+
+                return;
+            }
+
+            if (element.innerHTML === "") {
+                if (Alpine.cache.has(Alpine.constants.removedElement)) {
+                    element.innerHTML = Alpine.cache.get(
+                        Alpine.constants.removedElement
+                    );
+                }
+            }
+        },
     },
 
-    "x-show": (el, value) => {
-      el.style.display = value ? "block" : "none";
-    },
-    "x-if": (element, value) {
-      if (!value) {
-        if (element instanceof HTMLElement) {
-          if (!Alpine.cache.has(Alpine.constants.removedElement)) {
-            Alpine.cache.set(
-              Alpine.constants.removedElement,
-              element.innerHTML.toString()
-            );
-          }
+    start() {
+        this.root = document.querySelector("[x-data]");
+        this.rawData = this.getInitialData();
+        this.data = this.observe(this.rawData);
 
-          element.innerHTML = "";
+        this.refreshDom();
+        this.registerListeners();
+    },
+
+    getInitialData() {
+        let dataString = this.root.getAttribute("x-data");
+        return eval(`(${dataString})`);
+    },
+
+    observe(rawData) {
+        let self = this;
+        return new Proxy(rawData, {
+            set(target, key, value) {
+                target[key] = value;
+                self.refreshDom();
+                self.root.setAttribute("x-data", JSON.stringify(rawData));
+                return true;
+            },
+        });
+    },
+
+    refreshDom() {
+        this.walkDom(this.root, (el) => {
+            Array.from(el.attributes).forEach((attribute) => {
+                if (!Object.keys(this.directives).includes(attribute.name))
+                    return;
+
+                let directive = this.directives[attribute.name];
+                directive(el, eval(`with (this.data) { ${attribute.value} }`));
+            });
+        });
+    },
+
+    walkDom(el, callback) {
+        callback(el);
+
+        el = el.firstElementChild;
+
+        while (el) {
+            this.walkDom(el, callback);
+            el = el.nextElementSibling;
         }
-
-        return;
-      }
-
-      if (element.innerHTML === "") {
-        if (Alpine.cache.has(Alpine.constants.removedElement)) {
-          element.innerHTML = Alpine.cache.get(Alpine.constants.removedElement);
-        }
-      }
     },
-  },
 
-  start() {
-    this.root = document.querySelector("[x-data]");
-    this.rawData = this.getInitialData();
-    this.data = this.observe(this.rawData);
+    registerListeners() {
+        this.walkDom(this.root, (el) => {
+            this.addListenerToElement(el);
+        });
+    },
 
-    this.refreshDom();
-    this.registerListeners();
-  },
+    addListenerToElement(el) {
+        Array.from(el.attributes).forEach((attribute) => {
+            if (!attribute.name.startsWith("@")) return;
 
-  getInitialData() {
-    let dataString = this.root.getAttribute("x-data");
-    return eval(`(${dataString})`);
-  },
+            let event = attribute.name.substring(1);
 
-  observe(rawData) {
-    let self = this;
-    return new Proxy(rawData, {
-      set(target, key, value) {
-        target[key] = value;
-        self.refreshDom();
-        self.root.setAttribute("x-data", JSON.stringify(rawData));
-        return true;
-      },
-    });
-  },
-
-  refreshDom() {
-    this.walkDom(this.root, (el) => {
-      Array.from(el.attributes).forEach((attribute) => {
-        if (!Object.keys(this.directives).includes(attribute.name)) return;
-
-        let directive = this.directives[attribute.name];
-        directive(el, eval(`with (this.data) { ${attribute.value} }`));
-      });
-    });
-  },
-
-  walkDom(el, callback) {
-    callback(el);
-
-    el = el.firstElementChild;
-
-    while (el) {
-      this.walkDom(el, callback);
-      el = el.nextElementSibling;
-    }
-  },
-
-  registerListeners() {
-    this.walkDom(this.root, (el) => {
-      this.addListenerToElement(el);
-    });
-  },
-
-  addListenerToElement(el) {
-    Array.from(el.attributes).forEach((attribute) => {
-      if (!attribute.name.startsWith("@")) return;
-
-      let event = attribute.name.substring(1);
-
-      el.addEventListener(event, () => {
-        eval(`with(this.data) { ${attribute.value} }`);
-      });
-    });
-  },
+            el.addEventListener(event, () => {
+                eval(`with(this.data) { ${attribute.value} }`);
+            });
+        });
+    },
 };
 
 Alpine.start();
